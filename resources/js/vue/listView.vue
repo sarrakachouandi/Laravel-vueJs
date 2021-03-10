99d243e9e712f9579c589bb972a8d37131128768
Sarra kachouandi <sarrakachouandi@gmail.com>
2021-03-10 21:15:46 +0100

    Updateprob

---
+++ resources/js/vue/listView.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div v-for="(item, index) in items" :key="index">
+            <list-item
+                :item="item"
+                id="item"
+                v-on:itemchanged="$emit('relodlist')"
+            />
+        </div>
+    </div>
+</template>
+
+<script>
+import listItem from "./listItem";
+import ListItem from "./listItem.vue";
+export default {
+    props: ["items"],
+    components: {
+        listItem
+    }
+};
+</script>
+<style scoped>
+#item {
+    background: #e6e6e6;
+    padding: 5px;
+    margin-top: 5px;
+}
+</style>
